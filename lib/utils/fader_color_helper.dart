cade2171c95a5dd0d60becfd736ae14a51e1aea4
wilson.junior <wilson.fernandes@sistemainfo.com.br>
2025-11-04 19:45:02 -0300

    splash screen

--- lib/utils/fader_color_helper.dart
+++ lib/utils/fader_color_helper.dart
@@ -4,16 +4,12 @@ class FaderColorHelper {
 
   static Color getLevelColor(double level) {
     if (level < 0.30) {
-      // Muito baixo (próximo de -60dB ou menos) = Azul
       return Colors.blue;
-    } else if (level >= 0.30 && level <= 0.73) {
-      // Abaixo de 0dB = Amarelo
+    } else if (level >= 0.30 && level < 0.68) {
       return Colors.amber;
-    } else if (level >= 0.74 && level <= 0.76) {
-      // Em 0dB (com tolerância) = Verde
+    } else if (level >= 0.68 && level <= 0.82) {
       return Colors.green;
     } else {
-      // Acima de 0dB = Vermelho
       return Colors.red;
     }
   }
